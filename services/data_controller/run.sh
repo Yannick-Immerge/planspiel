c31fa7997992ece47c069c2c8d2a754d4e395b1d
Yannick Bergs <immerge.yannick@gmail.com>
2024-12-06 13:34:12 +0100

    [*] Fixed scripts

--- services/data_controller/run.sh
+++ services/data_controller/run.sh
@@ -3,3 +3,4 @@ if [ -z $PLANSPIEL_DATA_CONTROLLER_PORT ]; then
 else
   echo "Run Data controller on 0.0.0.0 and port: ${PLANSPIEL_DATA_CONTROLLER_PORT}"
   flask run --host 0.0.0.0 -p ${PLANSPIEL_DATA_CONTROLLER_PORT}
+fi
