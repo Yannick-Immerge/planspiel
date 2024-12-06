68ac5e4b1a48be7b613758f1ac0ed344ea862505
Yannick Bergs <immerge.yannick@gmail.com>
2024-12-06 13:49:34 +0100

    [*] Run in background and capture logs

--- services/game_controller/run.sh
+++ services/game_controller/run.sh
@@ -2,5 +2,5 @@ if [ -z $PLANSPIEL_GAME_CONTROLLER_PORT ]; then
   echo "WARN: The environment variable \"PLANSPIEL_GAME_CONTROLLER_PORT\" has to be configured."
 else
   echo "Run Data controller on 0.0.0.0 and port: ${PLANSPIEL_GAME_CONTROLLER_PORT}"
-  flask run --host 0.0.0.0 -p ${PLANSPIEL_GAME_CONTROLLER_PORT}
+  nohup flask run --host=0.0.0.0 --port=${PLANSPIEL_GAME_CONTROLLER_PORT} > flask.log 2>&1 &
 fi
