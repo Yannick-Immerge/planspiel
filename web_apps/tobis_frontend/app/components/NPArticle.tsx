3623cb4d741587b43544030cda0490402cb1c9d7
Rob <75586283+coding-with-rob@users.noreply.github.com>
2024-12-08 14:24:43 +0100

    Newspaper article with custom heading and text
    could be extended with article image

---
+++ web_apps/tobis_frontend/app/components/NPArticle.tsx
@@ -0,0 +1,17 @@
+interface Props {
+    children: string;
+    heading: string;
+}
+
+const NPArticle = ({ children, heading }: Props) => {
+    return (
+        <div>
+            <img src="src/assets/newspaper.jpg"></img>
+
+            <p className="article article-heading">{heading}</p>
+            <p className="article article-text"><br /><br />{children}</p>
+        </div>
+    )
+}
+
+export default NPArticle;
