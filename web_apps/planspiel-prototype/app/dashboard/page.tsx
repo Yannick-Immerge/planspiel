37cd14ece68f459a76a373fbf033d55a31813c9c
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-01-12 18:01:30 +0100

    A way to display the en roads link - please dont judge my aesthetics

--- web_apps/planspiel-prototype/app/dashboard/page.tsx
+++ web_apps/planspiel-prototype/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import {useEffect, useState} from "react";
-import {GamePhase, GameState, UserView} from "@/app/api/models";
+import { useEffect, useState } from "react";
+import { GamePhase, GameState, UserView } from "@/app/api/models";
 import {
     getGameState,
     getSessionMemberViews,
@@ -11,6 +11,7 @@ import WarningArea from "@/app/components/WarningArea";
 import MembersArea from "@/app/dashboard/MembersArea";
 import BuergerraeteArea from "@/app/dashboard/BuergerraeteArea";
 import TransitionArea, { StateDescription } from "@/app/dashboard/TransitionArea";
+import EnRoadsProjection from "@/app/dashboard/EnRoadsProjection";
 import { GetAllStateDescriptions } from "./StateDescriptions";
 
 
@@ -20,9 +21,12 @@ export default function Dashboard() {
     const [gameState, setGameState] = useState<GameState | null>(null);
     const [warning, setWarning] = useState<string | null>(null);
 
+    // This is needed to create the en-roads link to the generated dashboard
+    const [enRoadsURL, setEnRoadsURL] = useState<string | null>(null);
+
     const fetchUser = async () => {
         const viewResponse = await viewSelf();
-        if(!viewResponse.ok || viewResponse.data === null) {
+        if (!viewResponse.ok || viewResponse.data === null) {
             setUser(null);
             setWarning("Error: You are logged out!")
             return;
@@ -30,7 +34,7 @@ export default function Dashboard() {
         setUser(viewResponse.data.userView);
     }
 
-     const fetchMembers = async () => {
+    const fetchMembers = async () => {
         const viewResponse = await viewSelf();
         if (!viewResponse.ok || viewResponse.data === null) {
             setMembers(null);
@@ -45,14 +49,14 @@ export default function Dashboard() {
             return;
         }
 
-        
+
 
         setMembers(membersResponse.data.memberViews);
     }
 
     const fetchGameState = async () => {
         const gameStateResponse = await getGameState();
-        if(!gameStateResponse.ok || gameStateResponse.data === null) {
+        if (!gameStateResponse.ok || gameStateResponse.data === null) {
             setGameState(null);
             setWarning(`Error: ${gameStateResponse.statusText}`);
             return;
@@ -61,12 +65,44 @@ export default function Dashboard() {
         setGameState(gameStateResponse.data.gameState);
     }
 
+    const fetchEnRoadsURL = async () => {
+        const votingFinalized = true  // TODO: change this when done
 
+        if (!votingFinalized) {
+            setEnRoadsURL(null);
+            return;
+        }
+
+        // For now, we only hardcode this
+        const votingValues : any = {
+            "gases_agriculture": "100",
+            "reduction_meat": "10",
+            "reduction_waste": "10",
+            "fossil_fuel_taxes": "250",
+            "reduction_infra": "100",
+        }
+        const metricNameToParameter : any = {
+            "gases_agriculture": "60",
+            "reduction_meat": "287",
+            "reduction_waste": "305",
+            "fossil_fuel_taxes": "39",
+            "reduction_infra": "211",
+        }
+
+        // Now we create the URL
+        let url = "https://en-roads.climateinteractive.org/scenario.html?v=25.1.0"
+        for (const key in votingValues) {
+            url += `&p${metricNameToParameter[key]}=${votingValues[key]}`;
+        }
+
+        setEnRoadsURL(url);
+    }
 
     const revalidate = () => {
         fetchUser();
         fetchMembers();
         fetchGameState();
+        fetchEnRoadsURL();
     };
 
     useEffect(() => {
@@ -76,13 +112,13 @@ export default function Dashboard() {
     const onTransitionAction = (targetPhase: GamePhase) => {
         const performTransition = async (targetPhase: GamePhase) => {
             const result = await transitionGameState(targetPhase);
-            if(!result.ok || result.data === null) {
+            if (!result.ok || result.data === null) {
                 setWarning(result.statusText);
                 return;
             }
 
             const gameStateResult = await getGameState();
-            if(!gameStateResult.ok || gameStateResult.data === null) {
+            if (!gameStateResult.ok || gameStateResult.data === null) {
                 setWarning(result.statusText);
                 return;
             }
@@ -93,29 +129,30 @@ export default function Dashboard() {
     }
 
     const [stateDescriptions, setStateDescriptions] = useState<StateDescription[]>([]);
-    useEffect (() => {setStateDescriptions(GetAllStateDescriptions())}, [])
+    useEffect(() => { setStateDescriptions(GetAllStateDescriptions()) }, [])
 
     return (
         <>
-        <title>Planet Council Dashboard</title>
-        <div className="bg-cover bg-center bg-no-repeat bg-[url(/images/EarthTint.png)] min-h-screen bg-fixed">
-            <div className="absolute left-[8.33%] top-5 text-5xl">Admin-Dashboard</div>
-            <div className="pt-20 w-full">
-                <div className="flex h-80 justify-between gap-10 mx-10">
-                    <div className="w-1/2">
-                    <div className="flex-1 rounded-2xl bg-[#5a53] p-5 shadow-[10px_10px_10px_rgba(0,0,0,0.4)] backdrop-blur-xl">    
-                        <TransitionArea stateDescriptions={stateDescriptions} gameState={gameState} onTransitionAction={onTransitionAction}/>
+            <title>Planet Council Dashboard</title>
+            <div className="bg-cover bg-center bg-no-repeat bg-[url(/images/EarthTint.png)] min-h-screen bg-fixed">
+                <div className="absolute left-[8.33%] top-5 text-5xl">Admin-Dashboard</div>
+                <div className="pt-20 w-full">
+                    <div className="flex h-80 justify-between gap-10 mx-10">
+                        <div className="w-1/2">
+                            <div className="flex-1 rounded-2xl bg-[#5a53] p-5 shadow-[10px_10px_10px_rgba(0,0,0,0.4)] backdrop-blur-xl">
+                                <TransitionArea stateDescriptions={stateDescriptions} gameState={gameState} onTransitionAction={onTransitionAction} />
+                                <EnRoadsProjection enRoadsURL={enRoadsURL} />
+                            </div>
+                            <MembersArea members={members} />
+                        </div>
+                        <div className="w-1/2">
+                            <BuergerraeteArea gameState={gameState} users={members} />
+                        </div>
                     </div>
-                        <MembersArea members={members}/>
-                    </div>
-                    <div className="w-1/2">
-                        <BuergerraeteArea gameState={gameState} users={members}/>
-                    </div>
+
+                    <WarningArea warning={warning} />
                 </div>
-                
-                <WarningArea warning={warning}/>
             </div>
-        </div>
         </>
     );
 }
