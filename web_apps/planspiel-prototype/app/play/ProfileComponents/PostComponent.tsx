787bc1f21a0ed52b2aef54b16c034e57db291d63
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-01-14 19:55:35 +0100

    Fix issue of posts without image

--- web_apps/planspiel-prototype/app/play/ProfileComponents/PostComponent.tsx
+++ web_apps/planspiel-prototype/app/play/ProfileComponents/PostComponent.tsx
@@ -27,9 +27,11 @@ export default function PostComponent({ post, roleMetadata } : { post: Post, rol
     return (
     <div className={postFormat + " " + "bg-stone-200 m-auto b-l-8 py-1 rounded-2xl shadow-[0px_10px_30px_rgba(0,0,0,0.5)] max-w-[500px] text-slate-900 border-solid"} style={{"borderLeftWidth": "0px"}}>
         <div className=" text-[#0000009b] m-3">{descText}</div>
-        <div className="w-[100%] m-auto aspect-square shadow-[inset_0px_0px_20px_0px_rgba(1,1,1,1.0)]">
-            <div style={{"backgroundImage": `url(${post.imageIdentifiers[0]}`}} className="m-auto w-full h-full bg-cover"/>
-        </div>
+        {post.imageIdentifiers.length > 0 ? 
+            <div className="w-[100%] m-auto aspect-square shadow-[inset_0px_0px_20px_0px_rgba(1,1,1,1.0)]">
+                <div style={{"backgroundImage": `url(${post.imageIdentifiers[0]}`}} className="m-auto w-full h-full bg-cover"/>
+            </div>
+        : ""}
         <div className="mt-3 mx-3 select-none text-stone-500 text-xs">{showDE? (roleMetadata.language? ("Übersetzt von: " + roleMetadata.language) : "Auf Deutsch übersetzt") : ""}</div>
         <div onClick={() => setShowDE(!showDE)} className="mx-3 mb-3 text-stone-700 text-xs text-decoration-line: underline cursor-pointer select-none">{showDE? "Original anzeigen" : "Übersetzen"}</div>
         <div className="m-3">
