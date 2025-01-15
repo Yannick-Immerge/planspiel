35be130efa66cb03184be85f5faef6c5bece1b25
BisschenHeller <ge35vuz@mytum.de>
2025-01-15 16:41:27 +0100

    Virtually removed the front page, now redirects to login directly

--- web_apps/planspiel-prototype/app/page.tsx
+++ web_apps/planspiel-prototype/app/page.tsx
@@ -1,34 +1,18 @@
-import Link from "next/link";
-import ClientClickToRedirect from "./components/ClientClickToRedirect";
+"use client"
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
   
+  useEffect(() => {
+    router.push("/login");
+  }, []);
+    
   return (
     <main>
         <div className="bg-cover bg-center bg-no-repeat bg-[url(/images/EarthTint.png)] min-h-screen bg-fixed">
-            <div className="absolute left-[10%] w-4/5 pt-[5%] flex">
-                <div className="w-[60%]">
-                    <div className="rounded-2xl backdrop-blur-lg shadow-[10px_10px_10px_rgba(0,0,0,0.8)] p-8 ">
-                        <div className="text-left text-7xl text-lime-600">
-                            Heftiges Planspiel, Bro 
-                        </div>
-                        <div className="flex">
-                            <div className="pl-0 text-left text-6xl text-sky-400">
-                                Gemeinsam
-                            </div>
-                        <div>
-                            <div className="text-left text-2xl text-sky-400">
-                                für eine bessere Diskussionskultur.
-                            </div>
-                            <div className="text-left text-2xl text-sky-400">
-                                gegen den Klimawandel.
-                            </div>
-                        </div>
-                    </div>
-                  </div>
-                </div>
-                <ClientClickToRedirect text="Los geht's!" url="/login"/>
-            </div>
+            
         </div>
     </main>
   )
